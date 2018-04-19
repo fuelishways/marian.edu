be08858f3216dc4ea3c45b3ba88e44b5de6c2f50
Ed Ventura <ed.ventura@gmail.com>
2018-04-19 15:41:01 -0400

    remove rlets temp

--- _junk1.js
+++ _junk1.js
@@ -1,171 +1,73 @@
-<apex:page controller="AdmissionsCampaign2014Controller" showHeader="false" standardStylesheets="true" cache="false">
-<!--
-<apex:stylesheet value="http://it.marian.edu/SalesforceStyle/MarcomFormsStyle.css"/>
--->
+$(document).ready(function(){
+    $('.hiddencontent').hide();
+    $('.showhidebtn').click(function(e){
+        e.preventDefault();
+        $('.hiddencontent').slideDown(500);
+      $('.showhidebtn').hide();
+    });
+});
 
-<apex:stylesheet value="https://www.marian.edu/Sitefinity/WebsiteTemplates/Marian/App_Themes/lib/css/foundation.css"/>
-<apex:stylesheet value="https://www.marian.edu/Sitefinity/WebsiteTemplates/Marian/App_Themes/lib/css/speccss/salesforceformstyles.css"/>
-<!-- <apex:stylesheet value="http://www.marian.edu/Sitefinity/WebsiteTemplates/Marian/App_Themes/lib/css/app.css"/> -->
+$(document).ready(function(){
+    $('.rodgershiddencontent').hide();
+    $('.showhiderodgers').click(function(e){
+        e.preventDefault();
+        $('.rodgershiddencontent').slideDown(500);
+      $('.showhiderodgers').hide();
+    });
+});
 
+$(document).ready(function(){
+    $('.emceehiddencontent').hide();
+    $('.showhideemceebtn').click(function(e){
+        e.preventDefault();
+        $('.emceehiddencontent').slideDown(500);
+      $('.showhideemceebtn').hide();
+    });
+});
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W6XXPM');</script>
-<!-- End Google Tag Manager -->
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6XXPM"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+$(document).ready(function(){
+    $('.auction01hiddencontent').hide();
+    $('.showhideauction01').click(function(e){
+       $(this).parent().css('font-weight', '700');
+        e.preventDefault();
+        $('.auction01hiddencontent').slideDown(500);
+      $('.showhideauction01').hide();
+    });
+});
 
-
-<style>
-    div.errorMsg { display:none !important; }
-    .textareastreet {height: 14px;}
-</style>
-     <apex:outputPanel id="opMain" rendered="{!!submitted}">
-    <div class="content-right">
-<apex:form id="upupLandingForm" styleClass="upupLandingClass" >   
-    <apex:pageBlock >
-        
-        <div style="display:none">
-        <apex:pageMessages />
-        </div>
-        
-        <!--
-        <div class="row columns">
-            <img class="learnmorehereheader" src="https://www.marian.edu/images/default-source/_branding/learnmorehere-sfheader.png?sfvrsn=2" alt="learn more here."/>
-        </div>
-        -->
-        
-<!--  PERSONAL INFO    ------------------------------>
-        <div class="row">
-           <div class=" small-6 columns"> 
-               <label >First name
-                    <apex:inputField label="" value="{!theForm.First_Name__c}" required="true"/>
-               </label>
-            </div>
-            <div class=" small-6 columns">
-                <label>Last name
-                    <apex:inputField value="{!theForm.Last_Name__c}" label="" required="true"/>
-                </label>
-            </div>
-        </div>
-        <div class="row">
-            <div class=" small-6 columns"> 
-                <label>Email
-                    <apex:inputField label="" value="{!theForm.Email_Address__c}" required="true"/>
-                </label>
-            </div>
-            <div class=" small-6 columns"> 
-               <label>Phone
-                  <apex:inputField label="" value="{!theForm.Phone_Number__c}" required="true"/>
-               </label>
-           </div>
-        </div>
+$(document).ready(function(){
+    $('.auction02hiddencontent').hide();
+    $('.showhideauction02').click(function(e){
+        e.preventDefault();
+        $('.auction02hiddencontent').slideDown(500);
+      $('.showhideauction02').hide();
+    });
+});
 
 
-<!--  ADDRESS INFO    ------------------------------>
-        
-        <div class="row">
-           <div class=" small-12 columns"> 
-               <label>Street address
-                   <apex:inputField label="" value="{!theForm.Mailing_Street_Address__c}" required="true" styleClass="textareastreet"/>
-               </label>
-            </div>
-        </div>
-        <div class="row">
-           <div class=" small-6 columns"> 
-               <label>City
-                   <apex:inputField label="" value="{!theForm.Mailing_City__c}" required="true" styleClass="inputCityPlace" />
-               </label>
-            </div>
-            <div class=" small-3 columns"> 
-               <label>State
-                   <apex:inputField label="" value="{!theForm.Mailing_State__c}" required="true"/>
-               </label>
-            </div>
-            <div class=" small-3 columns"> 
-               <label>Zip
-                   <apex:inputField label="" value="{!theForm.Mailing_Zip_Code__c}" required="true"/>
-               </label>
-            </div>
-        </div>
-        <div class="row">
-                <div class=" small-6 columns"> 
-                   <label>High school grad year
-                      <apex:inputField label="" value="{!theForm.Graduation_Year__c}" required="true"/>
-                   </label>
-                </div>
-                <div class=" small-6 columns">
-                    <label> Are you currently...
-                        <apex:inputField value="{!theForm.Are_you_currently__c}"  required="true" /> 
-                    </label>
-                </div>
-        </div>
-        
-        <div class="row">
-                <div class=" small-6 columns">
-                    <label> Program of interest
-                        <apex:inputField value="{!theForm.ProgramOrSchoolOfInterest__c}"  required="true" /> 
-                    </label>
-                </div>
-                <div id="divButtons" class=" small-6 columns">
-                  <apex:commandButton action="{!submitForm}" value="Request info" styleClass="button mariangold expanded"></apex:commandButton>
-                 </div>
-        </div>
-        
-                        <!--  VISIT INFO    ------------------------------>
-                <!--
-                <apex:pageBlockSection columns="2" title="Tell us when you would like to attend…" collapsible="false">
-             
-                    <apex:repeat value="{!$ObjectType.Online_Form__c.FieldSets.Visit_Selection}" var="f">
-                        <apex:inputField value="{!theForm[f]}" required="{!f.Required}"/>
-                    </apex:repeat>
-                                                        
-                </apex:pageBlockSection>
-                
-    
-                 <br/>
-                 <b>Your visit is not scheduled until you receive a confirmation from the Office of Admissions.</b><br/>
-                 We will try to accommodate your first choice, but please list three options.<br/><br/>
-                 Individual campus visits are conducted Monday-Friday, starting at 9:30 a.m. and 1 p.m.<br/><br/>
-                 <b>An average visit lasts two hours.</b>
-                 -->
-         
-            
-                <!--  VISIT PURPOSE    ------------------------------>
-                <!--
-                <apex:pageBlockSection columns="1" title="During the campus visit, what would you like to do?" collapsible="false">
-                    <apex:repeat value="{!$ObjectType.Online_Form__c.FieldSets.Visit_Purpose}" var="f">
-                        <apex:inputField value="{!theForm[f]}" required="{!f.Required}"/>
-                    </apex:repeat>
-                </apex:pageBlockSection>   
-                -->    
-        
-    </apex:pageBlock>
-</apex:form>
-         </div>
-    </apex:outputPanel>
- <apex:outputPanel id="opThanks" rendered="{!submitted}">
-    <!--<div class="row column" style="margin-top:3rem;">
-        <p class="thankYouMsg" style="font-size:22px; line-height:1; font-weight:700;">Thank you for your interest in Marian University!</p>
-        <p style="font-size:16px;"> We will be in contact with you soon.</p>
-        <p style="text-align:center;">
-            <img src="http://www.marian.edu/images/default-source/_branding/muknightslogowhiterball-130px.png?sfvrsn=2" alt="marian university" />
-        </p>
-    </div>-->
+$(document).ready(function(){
+    $('.auction03hiddencontent').hide();
+    $('.showhideauction03').click(function(e){
+        e.preventDefault();
+        $('.auction03hiddencontent').slideDown(500);
+      $('.showhideauction03').hide();
+    });
+});
 
-       <script type="text/javascript">
-            /* window.parent.location.href = 'http://www.marian.edu/lp/thank-you'; */
-            window.location.href = 'http://marian.edu/_confirmations/findout-confirmation'; 
-        </script> 
+$(document).ready(function(){
+    $('.auction04hiddencontent').hide();
+    $('.showhideauction04').click(function(e){
+        e.preventDefault();
+        $('.auction04hiddencontent').slideDown(500);
+      $('.showhideauction04').hide();
+    });
+});
 
-    
-    
-    </apex:outputPanel>
- 
-    
-</apex:page>
+$(document).ready(function(){
+    $('.auction05hiddencontent').hide();
+    $('.showhideauction05').click(function(e){
+        e.preventDefault();
+        $('.auction05hiddencontent').slideDown(500);
+      $('.showhideauction05').hide();
+    });
+});
