c49b6eb49f2747e3e3aecd5153f338aa58b534c5
Ed Ventura <ed.ventura@gmail.com>
2018-11-16 10:55:35 -0500

    edits to master pages

--- _junk1.js
+++ _junk1.js
@@ -1,73 +1,10 @@
-$(document).ready(function(){
-    $('.hiddencontent').hide();
-    $('.showhidebtn').click(function(e){
-        e.preventDefault();
-        $('.hiddencontent').slideDown(500);
-      $('.showhidebtn').hide();
-    });
-});
-
-$(document).ready(function(){
-    $('.rodgershiddencontent').hide();
-    $('.showhiderodgers').click(function(e){
-        e.preventDefault();
-        $('.rodgershiddencontent').slideDown(500);
-      $('.showhiderodgers').hide();
-    });
-});
-
-$(document).ready(function(){
-    $('.emceehiddencontent').hide();
-    $('.showhideemceebtn').click(function(e){
-        e.preventDefault();
-        $('.emceehiddencontent').slideDown(500);
-      $('.showhideemceebtn').hide();
-    });
-});
-
-$(document).ready(function(){
-    $('.auction01hiddencontent').hide();
-    $('.showhideauction01').click(function(e){
-       $(this).parent().css('font-weight', '700');
-        e.preventDefault();
-        $('.auction01hiddencontent').slideDown(500);
-      $('.showhideauction01').hide();
-    });
-});
-
-$(document).ready(function(){
-    $('.auction02hiddencontent').hide();
-    $('.showhideauction02').click(function(e){
-        e.preventDefault();
-        $('.auction02hiddencontent').slideDown(500);
-      $('.showhideauction02').hide();
-    });
-});
-
-
-$(document).ready(function(){
-    $('.auction03hiddencontent').hide();
-    $('.showhideauction03').click(function(e){
-        e.preventDefault();
-        $('.auction03hiddencontent').slideDown(500);
-      $('.showhideauction03').hide();
-    });
-});
-
-$(document).ready(function(){
-    $('.auction04hiddencontent').hide();
-    $('.showhideauction04').click(function(e){
-        e.preventDefault();
-        $('.auction04hiddencontent').slideDown(500);
-      $('.showhideauction04').hide();
-    });
-});
-
-$(document).ready(function(){
-    $('.auction05hiddencontent').hide();
-    $('.showhideauction05').click(function(e){
-        e.preventDefault();
-        $('.auction05hiddencontent').slideDown(500);
-      $('.showhideauction05').hide();
-    });
-});
+$(document).ready(
+   function ampmfix() {
+      $(".eventtime").text(function (index, text) {
+         return text.replace(':00', '');
+      });
+      $(".eventtime").text(function (index, text) {
+         return text.replace('12 a.m.', 'ALL DAY');
+      });
+      setTimeout(ampmfix, 2000);
+   });
