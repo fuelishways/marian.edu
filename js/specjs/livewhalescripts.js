cf20eeafe6aa5b6b4153f12a29f2fd7278760634
Ed Ventura <ed.ventura@gmail.com>
2019-01-08 14:02:58 -0500

    update livewhalescripts.js to fix time formatting

--- js/specjs/livewhalescripts.js
+++ js/specjs/livewhalescripts.js
@@ -2,14 +2,17 @@
 
 $(document).ready(
   function brandedAmPm() {
+    // $(".startTimeBrandFormat").text(function() {
+    //   return $(this).text().replace("AM", "a.m.");
+    // });
     $(".startTimeBrandFormat").text(function() {
-      return $(this).text().replace("AM", "a.m.");
+      return $(this).text().replace("12:00 p.m.", "noon");
     });
     $(".startTimeBrandFormat").text(function() {
-      return $(this).text().replace("12 PM", "noon");
+      return $(this).text().replace(":00","");
     });
-    $(".startTimeBrandFormat").text(function() {
-      return $(this).text().replace("PM", "p.m.");
-    });
+    // $(".startTimeBrandFormat").text(function() {
+    //   return $(this).text().replace("PM", "p.m.");
+    // });
     setTimeout(brandedAmPm, 3000);
   });
