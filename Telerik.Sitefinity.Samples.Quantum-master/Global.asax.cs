8e614b2362233ee42b68c47fa58cf93d133426a1
ed ventura <ed.ventura@gmail.com>
2021-06-04 15:37:40 -0400

    sitefinity quantum bootstrap4 packageresource

---
+++ Telerik.Sitefinity.Samples.Quantum-master/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Helpers;
+using Telerik.Microsoft.Practices.Unity;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Routing;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Services;
+
+namespace SitefinityWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("X-Frame-Options", "SAMEORIGIN");
+        }
+
+        protected void Application_Start()
+        {
+            AntiForgeryConfig.SuppressXFrameOptionsHeader = true;
+        }
+    }
+}
